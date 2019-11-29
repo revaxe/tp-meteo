af249a1a79270b6c46cafe96766ee1ff56ef696e
axel.leon.29@gmail.com <wasMSceI29>
2019-11-29 17:55:15 +0100

    Ajout clear city

--- src/components/SearchCity.vue
+++ src/components/SearchCity.vue
@@ -4,7 +4,7 @@
             <form @submit="searchCity" method="post" class="find-location">
                 <Autocomplete :propositions="cities" :select="selectCity" :transformText="getLabelCity">
                     <input slot="input" v-model="form.city"
-                           placeholder="Trouver votre ville..." type="text"
+                           placeholder="Trouver votre ville..." type="text" v-focus
                            :class="{'error': form.city === '' && city !== undefined}">
                 </Autocomplete>
                 <input type="submit" value="Rechercher">
@@ -33,7 +33,7 @@
         methods: {
             async searchCity(e) {
                 if (e) e.preventDefault();
-                if (!this._.isEmpty(this.form.city)) {
+                if (!this._.isEmpty(this.form.city) && !this._.isEqual(this.form.city, this.getLabelCity(this.city))) {
                     let cities = await LocationService.searchCity(this.form.city);
                     if (cities.length > 1 || !(cities.length === 1 && this.city && cities[0].locationId === this.city.locationId))
                         this.cities = cities;
@@ -50,20 +50,27 @@
                 if (newCityName && !this._.isEqual(newCityName, this.getLabelCity(this.city))) {
                     this.$store.dispatch('clearCity');
                     this.debouncedSearchCity();
-                }
-                else if (newCityName.length === 0) {
+                } else if (newCityName.length === 0) {
                     this.cities = undefined;
                     this.$store.dispatch('clearCity');
                 }
             },
             'city': function (newCity) {
-                this.form.city = this.getLabelCity(newCity);
+                if (newCity)
+                    this.form.city = this.getLabelCity(newCity);
             }
         },
         created: function () {
             // _.debounce est une fonction fournie par lodash pour limiter la fréquence
             // d'exécution d'une opération particulièrement couteuse.
             this.debouncedSearchCity = this._.debounce(this.searchCity, 400)
+        },
+        directives: {
+            focus: {
+                inserted: function (el) {
+                    el.focus()
+                }
+            }
         }
     }
 </script>
