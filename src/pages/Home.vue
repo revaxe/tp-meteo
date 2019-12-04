4f8fa72825b7d1811984b18df5302513bc7e8ede
axel.leon.29@gmail.com <wasMSceI29>
2019-12-04 10:35:26 +0100

    Init tp1.1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
             <div class="container">
                 <form v-on:submit.prevent="search" method="post" class="find-location" id="recherche">
                     <input v-model="form.city" placeholder="Trouver votre ville..." type="text">
-                    <input type="submit" value="Rechercher">
+                    <input type="submit" value="Rechercher" :disabled="isDisabled">
                 </form>
             </div>
         </div>
@@ -23,8 +23,14 @@
                 }
             }
         },
+        computed: {
+            isDisabled() {
+                return this.form.city === undefined || this.form.city === '';
+            }
+        },
         methods: {
             search() {
+                console.log('search');
                 console.log(this.form.city)
             }
         }
