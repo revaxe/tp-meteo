beff334e2be28b0c80b049e40fc2a21e8337ae3e
revaxe <axel.leon.29@gmail.com>
2020-09-05 21:42:43 +0200

    Fix TP 1.1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './filters'
 import WeatherUiKit from './plugins/weather-ui-kit';
 
 Vue.config.productionTip = false;
